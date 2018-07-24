97ca2b350f2382ac8d978eaf4fce3c51d7a80ca9
Nick Drishinski <ndrishinski@yahoo.com>
2018-07-24 17:42:39 -0600

    created contact component

---
+++ src/components/contacts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactsList extends React.Component {
+    render() {
+        return (
+            <ul>
+                <li>Scott 435-232-3233</li>
+            </ul>
+        )
+    }
+}
+
+export default ContactsList;
